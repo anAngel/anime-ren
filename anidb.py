d4e5490979c862e84dae0a1cf767ccfbbbf188dd
Rusty Shackleford <gigolo@hotmail.co.uk>
2016-10-12 00:43:00 +0100

    auth isn't returning a session???

--- anidb.py
+++ anidb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys, re, socket, atexit, threading, time, os.path
 
-host       = ('api.anidb.net', 9000)
+host       = ("api.anidb.net", 9000)
 port       = 1444
 file_arr   = []
 still_open = True
@@ -16,7 +16,7 @@ class ED2K():
 
 class Response():
     def __init__(self, msg):
-        a = msg.split(" ")
+        a = msg.split(' ')
         self.code = int(a[0])
         self.msg  = ' '.join(a[1:])
 
@@ -31,8 +31,14 @@ class Config():
             print("ERROR! Username or password not proved in config")
             exit()
 
-        self.fmask = self.make_map(['unused', 'aid', 'eid', 'gid', 'mylist_id', 'list_other_episodes', 'IsDeprecated', 'state', 'size', 'ed2k', 'md5', 'sha1', 'crc32', 'unused', 'unused', 'reserved', 'quality', 'src', 'audio', 'audio_bitrate_list', 'video', 'video_bitrate', 'res', 'file_type', 'dub', 'sub', 'length_in_seconds', 'description', 'aired_date', 'unused', 'unused', 'anidb_file_name', 'mylist_state', 'mylist_filestate', 'mylist_viewed', 'mylist_viewdate', 'mylist_storage', 'mylist_source', 'mylist_other', 'unused'], fields)
-        self.amask = self.make_map(['anime_total_episodes', 'highest_episode_number', 'year', 'file_type', 'related_aid_list', 'related_aid_type', 'category_list', 'reserved', 'romanji_name', 'kanji_name', 'english_name', 'other_name', 'short_name_list', 'synonym_list', 'retired', 'retired', 'epno', 'ep_name', 'ep_romanji_name', 'ep_kanji_name', 'episode_rating', 'episode_vote_count', 'unused', 'unused', 'group_name', 'group_short_name', 'unused', 'unused', 'unused', 'unused', 'unused', 'date_aid_record_updated'], fields)
+        fmask = self.make_map(['unused', 'aid', 'eid', 'gid', 'mylist_id', 'list_other_episodes', 'IsDeprecated', 'state', 'size', 'ed2k', 'md5', 'sha1', 'crc32', 'unused', 'unused', 'reserved', 'quality', 'src', 'audio', 'audio_bitrate_list', 'video', 'video_bitrate', 'res', 'file_type', 'dub', 'sub', 'length_in_seconds', 'description', 'aired_date', 'unused', 'unused', 'anidb_file_name', 'mylist_state', 'mylist_filestate', 'mylist_viewed', 'mylist_viewdate', 'mylist_storage', 'mylist_source', 'mylist_other', 'unused'], fields)
+        amask = self.make_map(['anime_total_episodes', 'highest_episode_number', 'year', 'file_type', 'related_aid_list', 'related_aid_type', 'category_list', 'reserved', 'romanji_name', 'kanji_name', 'english_name', 'other_name', 'short_name_list', 'synonym_list', 'retired', 'retired', 'epno', 'ep_name', 'ep_romanji_name', 'ep_kanji_name', 'episode_rating', 'episode_vote_count', 'unused', 'unused', 'group_name', 'group_short_name', 'unused', 'unused', 'unused', 'unused', 'unused', 'date_aid_record_updated'], fields)
+        self.fmask  = fmask[0]
+        self.amask  = amask[0]
+        self.fields = ['fid'] + fmask[1] + amask[1]
+
+    def get(self, key):
+        return self.args[key] if key in self.args else None
 
     def make_map(self, a, b):
         m = ''.join(['1' if x in b else '0' for x in a])
@@ -41,8 +47,9 @@ class Config():
 
 
 class API(threading.Thread):
-    def __init__(self):
+    def __init__(self, config):
         threading.Thread.__init__(self)
+        self.config  = config
         self.timer   = int(time.time())
         self.session = ""
         try:
@@ -64,18 +71,25 @@ class API(threading.Thread):
             self.send("LOGOUT s=%s" % self.session, True)
         self.s.close()
 
-    def auth(self):
-        user, pw = open('secret', 'r').read()[:-1].split('\n')
-        ret = self.send("AUTH user=%s&pass=%s&protover=3&client=aniren&clientver=3&nat=1&enc=utf-8" % (user, pw), True)
-        if ret.code == 200:
-            return ret.msg.split(" ")[0]
+    def auth(self, auth_try=0):
+        ret = self.send("AUTH user=%s&pass=%s&protover=3&client=aniren&clientver=3&nat=1&enc=utf-8" % (self.config.get('user'), self.config.get('pass')), not auth_try)
+        if ret.code != 200:
+            print("ERROR! Auth failed")
+            exit()
+        
+        session = ret.msg.split(' ')[0]
+        m = re.match(r'^[a-z0-9]{4,8}$', session, re.I)
+        if m:
+            return session
         else:
-            print("ERROR! Auth failed: %d %s" % (ret.code, ret.msg))
+            print("ERROR! Auth failed, can't get session key")
             exit()
 
     def file(self, ed2k):
-        ret = self.send('FILE size=%d&ed2k=%s&fmask=%s&amask=%s&s=%s' % (ed2k.size, ed2k.hash, fmask, amask, self.session))
-        return
+        ret = self.send("FILE size=%d&ed2k=%s&fmask=%s&amask=%s&s=%s" % (ed2k.size, ed2k.hash, self.config.fmask, self.config.amask, self.session))
+        x   = ret.msg.split('|')
+        y   = {f: x[self.config.fields.index(f)] for f in fields}
+        print(y)
 
     def update_timer(self):
         self.timer = int(time.time())
@@ -93,8 +107,13 @@ class API(threading.Thread):
         print("< %d %s" % (a.code, a.msg[:-1]))
         return a
 
-c = Config("secret")
-api = API()
+c_path = os.path.expanduser("~/.anime-ren.conf")
+if not os.path.exists(c_path):
+    c_path = ".anime-ren.conf"
+    if not os.path.exists(c_path):
+        print("ERROR! Can't find config file")
+        exit()
+api = API(Config(c_path))
 api.start()
 
 def at_exit():
